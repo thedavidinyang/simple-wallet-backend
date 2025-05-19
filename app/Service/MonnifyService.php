c2687974e68464a8b923605a69ca9084faf35b60
David Inyang <davidinyang01@gmail.com>
2025-05-19 12:52:55 +0100

    veritrans init

--- app/Service/MonnifyService.php
+++ app/Service/MonnifyService.php
@@ -17,7 +17,16 @@ class MonnifyService {
 
     public function verifyTransaction($transactionId)
     {
-        $url = config('monnify.api_url') . '/v1/merchant/transactions/' . $transactionId;
+
+        try{
+
+            $url = config('monnify.api_url') . '/v1/merchant/transactions/' . $transactionId;
+        }
+        catch(\Exception $e){
+
+            throw new \Exception('Error: ' . $e->getMessage());
+          
+        }
 
 
     }
