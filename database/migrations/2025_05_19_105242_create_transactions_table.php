1ea0d32fc8e5cbb8725f9d86c69bbac222728de2
David Inyang <davidinyang01@gmail.com>
2025-05-20 21:00:14 +0100

    updater

--- database/migrations/2025_05_19_105242_create_transactions_table.php
+++ database/migrations/2025_05_19_105242_create_transactions_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('transactions', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            $table->decimal('amount', 20, 8);
+            $table->decimal('amount', 20, 2);
             $table->enum('type', ['credit', 'debit']);
             $table->enum('status', ['pending', 'completed', 'failed']);
             $table->string('transaction_id')->unique();
